ef5f3d92022e949c0d071569bdb2ef4f6c5221b6
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-13 23:00:43 -0800

    commit tree f16b6107f46412aacbbc8929e742d7543828933f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i=0
+  dwarves.each_with_index do |name|
+    puts "#{i+=1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect do |name|
+    name.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(verb)
+  answer=false
+  verb.collect do |call|
+  if call.length > 4
+    answer=true
+  end
+end
+  answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredient)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredient.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
